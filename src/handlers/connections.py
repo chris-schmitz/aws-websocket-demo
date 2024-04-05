e660e8e0ecf483e4c5e357036e64805e2784d7e0
Christopher Schmitz <schmitz.chris@gmail.com>
2024-04-05 17:14:24 -0500

    OH MY GOD FINALLY GOT BOTO MOCKED CORRECTLY :dies:

--- src/handlers/connections.py
+++ src/handlers/connections.py
@@ -2,86 +2,82 @@ import json
 import os
 from dataclasses import dataclass
 from os import environ
-from boto3 import client
-
-dynamo = client("dynamodb")
-
-connections_table_name = environ.get("CONNECTIONS_TABLE_NAME")
+import boto3
 
 
-def store_connection(connection_id):
-    return dynamo.put_item(
-        TableName=connections_table_name, Item={"connection_id": {"S": connection_id}}
-    )
+class ConnectionLogger:
+    def __init__(self):
+        self.connections_table_name = environ.get("CONNECTIONS_TABLE_NAME")
+        self.dynamo = boto3.client('dynamodb')
 
+    def store_connection(self, connection_id):
+        return self.dynamo.put_item(
+            TableName=self.connections_table_name, Item={"connection_id": {"S": connection_id}}
+        )
 
-def delete_connection(connection_id):
-    return dynamo.delete_item(
-        TableName=connections_table_name, Key={"connection_id": {"S": connection_id}}
-    )
+    def delete_connection(self, connection_id):
+        return self.dynamo.delete_item(
+            TableName=self.connections_table_name, Key={"connection_id": {"S": connection_id}}
+        )
 
+    def store_login(self, connection_id, email):
+        return self.dynamo.update_item(
+            TableName=self.connections_table_name,
+            Key={"connection_id": {"S": connection_id}},
+            UpdateExpression=" SET email = :email",
+            ExpressionAttributeValues={":email": {"S": email}},
+        )
 
-def store_login(connection_id, email):
-    return dynamo.update_item(
-        TableName=connections_table_name,
-        Key={"connection_id": {"S": connection_id}},
-        UpdateExpression=" SET email = :email",
-        ExpressionAttributeValues={":email": {"S": email}},
-    )
-
-
-def handle_connect(event, context):
-    print("========================")
-    print("stored record")
-    print(event)
-    # TODO: now that this handles both connect and disconnect
-    try:
-        connection_id = event["requestContext"]["connectionId"]
-        print("------------------------")
-        print(event["requestContext"]["connectionId"])
-        print("------------------------")
-        print("Connection id:")
-        print(connection_id)
-        result = store_connection(connection_id)
-        print(result)
-        return {"statusCode": 200, "body": json.dumps({"message": "ur connected!"})}
-    except RuntimeError as e:
-        print("exception")
-        print(e)
-        return {"statusCode": 500, "body": json.dumps({"error": e})}
-
-
-def handle_disconnect(event, context):
-    print("xxxxxxxxxxxxxxxxxxxxxxxx")
-    print("disconnect")
-    print(event)
-    try:
-        connection_id = event["requestContext"]["connectionId"]
-        print("------------------------")
-        print(event["requestContext"]["connectionId"])
-        print("------------------------")
-        print("Connection id:")
-        print(connection_id)
-        result = delete_connection(connection_id)
-        print(result)
-        return {"statusCode": 200, "body": json.dumps({"message": "ur connected!"})}
-    except RuntimeError as e:
-        print("exception")
-        print(e)
-        return {"statusCode": 500, "body": json.dumps({"error": e})}
-
-
-def handle_login(event, context):
-    try:
-        print("^^^^^^^^^^^^^^")
+    def handle_connect(self, event, context):
+        print("========================")
+        print("stored record")
         print(event)
-        connection_id = event["requestContext"]["connectionId"]
-        email = event["body"]["email"]
-        print([connection_id, email])
-        result = store_login(connection_id, email)
-        print(result)
-        return {"statusCode": 200, "body": json.dumps({"message": "ur connected!"})}
-    except RuntimeError as e:
-        print("exception")
-        print(e)
-        return {"statusCode": 500, "body": json.dumps({"error": e})}
+        # TODO: now that this handles both connect and disconnect
+        try:
+            connection_id = event["requestContext"]["connectionId"]
+            print("------------------------")
+            print(event["requestContext"]["connectionId"])
+            print("------------------------")
+            print("Connection id:")
+            print(connection_id)
+            result = self.store_connection(connection_id)
+            print(result)
+            return {"statusCode": 200, "body": json.dumps({"message": "ur connected!"})}
+        except RuntimeError as e:
+            print("exception")
+            print(e)
+            return {"statusCode": 500, "body": json.dumps({"error": e})}
+
+    def handle_disconnect(self, event, context):
+        print("xxxxxxxxxxxxxxxxxxxxxxxx")
+        print("disconnect")
+        print(event)
+        try:
+            connection_id = event["requestContext"]["connectionId"]
+            print("------------------------")
+            print(event["requestContext"]["connectionId"])
+            print("------------------------")
+            print("Connection id:")
+            print(connection_id)
+            result = self.delete_connection(connection_id)
+            print(result)
+            return {"statusCode": 200, "body": json.dumps({"message": "ur connected!"})}
+        except RuntimeError as e:
+            print("exception")
+            print(e)
+            return {"statusCode": 500, "body": json.dumps({"error": e})}
+
+    def handle_login(self, event, context):
+        try:
+            print("^^^^^^^^^^^^^^")
+            print(event)
+            connection_id = event["requestContext"]["connectionId"]
+            email = event["body"]["email"]
+            print([connection_id, email])
+            result = self.store_login(connection_id, email)
+            print(result)
+            return {"statusCode": 200, "body": json.dumps({"message": "ur connected!"})}
+        except RuntimeError as e:
+            print("exception")
+            print(e)
+            return {"statusCode": 500, "body": json.dumps({"error": e})}
--- test/test_connections.py
+++ test/test_connections.py
@@ -2,33 +2,35 @@ import json
 from unittest.mock import patch, MagicMock
 import pytest
 
-from src.handlers.connections import handle_login
+from src.handlers.connections import ConnectionLogger
 
 
-@patch.dict("os.environ", {"CONNECTIONS_TABLE_NAME": "some-table"})
-@patch("src.handlers.connections.client", name="boto_client_function_mock")
-def test_can_store_login_information(boto_client_function_mock):
-    mock_dynamo_client = MagicMock(name="dynamo-client-mock")
-    boto_client_function_mock.return_value = mock_dynamo_client
-    expected_connection_id = "some-connection-id"
-    expected_email = "coolguy@important.biz"
-    event = read_in_resource_file("login_event.json")
-    event_body = json.loads(event["body"])
-    event["requestContext"]["connectionId"] = expected_connection_id
-    event_body["email"] = expected_email
-    event_body["action"] = "login"
-    event["body"] = event_body
+class TestSuite:
 
-    handle_login(event, {})
+    @patch.dict("os.environ", {"CONNECTIONS_TABLE_NAME": "some-table"})
+    @patch("src.handlers.connections.boto3", name="mock_dynamo_client")
+    def test_can_store_login_information(self, mock_boto):
+        mock_dynamo_client = MagicMock(name="dynamo-client-mock")
+        mock_boto.client.return_value = mock_dynamo_client
+        expected_connection_id = "some-connection-id"
+        expected_email = "coolguy@important.biz"
+        event = self.read_in_resource_file("login_event.json")
+        event_body = json.loads(event["body"])
+        event["requestContext"]["connectionId"] = expected_connection_id
+        event_body["email"] = expected_email
+        event_body["action"] = "login"
+        event["body"] = event_body
+        connection_logger = ConnectionLogger()
 
-    mock_dynamo_client.update_item.assert_called_with(
-        TableName="some-table",
-        Key={"connection_id": {"S": expected_connection_id}},
-        UpdateExpression=" SET email = :email",
-        ExpressionAttributeValues={":email": {"S": expected_email}},
-    )
+        connection_logger.handle_login(event, {})
 
+        mock_dynamo_client.update_item.assert_called_with(
+            TableName="some-table",
+            Key={"connection_id": {"S": expected_connection_id}},
+            UpdateExpression=" SET email = :email",
+            ExpressionAttributeValues={":email": {"S": expected_email}},
+        )
 
-def read_in_resource_file(file_name: str):
-    with open(f"./resources/{file_name}", "r") as file:
-        return json.load(file)
+    def read_in_resource_file(self, file_name: str):
+        with open(f"./resources/{file_name}", "r") as file:
+            return json.load(file)
